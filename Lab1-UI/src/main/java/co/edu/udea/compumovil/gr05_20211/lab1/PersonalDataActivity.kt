c65fc651da3645f875976591d9fa4b2dd5f3b887
edisonbedoya98 <edison.bedoyag@udea.edu.co>
2021-04-20 21:17:29 -0500

    Actualiza avance del formulario

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20211/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20211/lab1/PersonalDataActivity.kt
@@ -1,7 +1,11 @@
 package co.edu.udea.compumovil.gr05_20211.lab1
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class PersonalDataActivity : AppCompatActivity() {
 
@@ -13,5 +17,25 @@ class PersonalDataActivity : AppCompatActivity() {
         actionBar!!.title = "Información Personal"
 
         actionBar.setDisplayHomeAsUpEnabled(true)
+
+        //Open calendar
+        val mPickTimeBtn = findViewById<Button>(R.id.change_btn)
+        val textView     = findViewById<TextView>(R.id.dateTv)
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        mPickTimeBtn.setOnClickListener {
+
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in TextView
+                textView.setText("" + dayOfMonth + " " + month + ", " + year)
+            }, year, month, day)
+            dpd.show()
+
+        }
+
     }
 }
